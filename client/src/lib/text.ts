49918804f891cfc60c0298a8155443a903448757
Junsung Lee <jjoons2010@gmail.com>
2026-03-02 00:08:08 +0900

    restart this project

--- client/src/lib/text.ts
+++
@@ -1,5 +0,0 @@
-export function preview(value: string): string {
-  const normalized = value.replace(/\r\n/g, '\n').replace(/\r/g, '\n');
-  const escaped = normalized.replace(/\n/g, '\\n').replace(/\t/g, '\\t');
-  return escaped.length > 0 ? escaped : '(empty)';
-}
